641c592519b12f45cabce2c4faabbc57d05c2219
novrizaldi <novri0737@gmail.com>
2020-09-28 22:09:34 +0700

    baru ingat satu lagi

---
+++ c16.js
@@ -0,0 +1,72 @@
+
+class Tyre {
+    constructor (merk, size){
+        this.merk = merk;
+        this.size = size;
+    }ca
+}
+
+class Car {
+    constructor(merkBan, ukuranBan, kursi, pintu, tahun, warranty ) {
+        this.ban = new Tyre (merkBan, ukuranBan);
+        this.tahun = tahun;
+        this.kursi = kursi;
+        this.pintu = pintu;
+        this.warranty = warranty;
+    }
+}
+
+class Fortuner extends Car {
+    constructor(tahun){
+    super('dunlop', '200/70', 8, 4, tahun);
+    this.varian = 'fortuner';
+    this.warranty = 3;
+    }
+}
+
+class Pajero extends Car {
+    constructor(tahun) {
+        super('Bridgeston', '200/110', 8, 4, tahun);
+        this.varian = 'Pajero';
+        this.warranty = 5;
+    }
+}
+
+class CarFactory {
+    constructor (){
+        this.Car = [];
+    }
+    static random (){
+        return Math.floor(Math.random() * 5 ) 
+    }
+
+    Produksi (tahun) {
+        for (let i = 0; i < CarFactory.random(); i++) {
+            this.Car.push(new Fortuner(tahun))            
+        }
+        for (let i = 0; i < CarFactory.random(); i++) {
+            this.Car.push(new Pajero(tahun))
+        }
+    }
+    Garansi (tahunSimulasi){
+        console.log('hasil produksi ');
+        this.Car.forEach (element => {
+            console.log(` jenis varian : ${element.varian}
+                            tahun pembuatan : ${element.tahun}
+                            jumlah pintu : ${element.pintu}
+                            jumlah kursi : ${element.kursi}
+                            Ban          : merk :${element.ban.merk}, ukuran : ${element.ban.size}
+                            garansi     : ${element.warranty} tahun
+                            status garansi : ${(tahunSimulasi - element.tahun) > element.warranty ? 'expired' : 'aktif'} 
+                            `);
+            
+        });
+           
+    }
+}
+
+let mobil = new CarFactory();
+mobil.Produksi(2020);
+// mobil.Produksi(2020);
+mobil.Garansi(2023);
+
